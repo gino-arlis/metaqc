5a521e0fd33090a2e28d2c3b74aab06a43e9935d
gino serpa <gserpa@arlis.umd.edu>
2024-01-17 18:36:29 -0600

    a graph

--- analyze_wos.py
+++ analyze_wos.py
@@ -3,8 +3,10 @@ import pandas as pd
 import plotly.express as px
 import pickle
 from collections import defaultdict
+import networkx as nx
+import itertools
 
-#color_discrete_map= defaultdict('yellow')
+color_discrete_map= defaultdict(lambda:'yellow')
 color_discrete_map = {'USA':'blue',
                       'Canada': 'cyan',
                       'Germany': 'lightblue',
@@ -80,3 +82,80 @@ def get_fig_prolific_authors(authors_d, n=10):
                               'categoryarray':df.index})
     
     return fig
+
+
+def get_prolific_institutions(institutions_d):
+
+    prolific_institutions = list()
+
+    for institution in institutions_d:
+        # number of papers
+        n_papers = len(institutions_d[institution]['papers'])
+
+        # Country
+        #print(institutions_d[institution])
+        country = institutions_d[institution]['country']
+
+        prolific_institutions.append((institution, n_papers, country))
+    prolific_institutions = sorted(prolific_institutions, 
+                                   key= lambda item: item[1], 
+                                   reverse=True)
+
+    return prolific_institutions
+
+
+def get_fig_prolific_institutions(institutions_d, n=10):
+
+    prolific_institutions = get_prolific_institutions(institutions_d)
+    prolific_institutions = prolific_institutions[0:n]
+
+    # Create df for plotly
+    institution = [item[0] for item in prolific_institutions]
+    institution_short_name=[item[0:15] for item in institution]
+
+    count = [item[1] for item in prolific_institutions]
+    country = [item[2] for item in prolific_institutions]
+
+    df = pd.DataFrame({'institution': institution, 
+                       'institution_short_name': institution_short_name,
+                       'count': count, 
+                       'country':country})
+    
+
+    fig = px.bar(df, 
+                 y='institution_short_name', 
+                 color = 'country', 
+                 x= 'count', 
+                 orientation = 'h',
+                 labels = {'count': 'number of papers'},
+                 color_discrete_map= color_discrete_map,
+                 title = 'Most prolific institutions')
+    
+    fig.update_layout( yaxis={'categoryorder':'total descending', 
+                              'categoryarray':df.index},
+                              margin=dict(l=100))
+    
+    return fig
+
+
+def get_inst_graph(institutions_d, cut_off = 20):
+    G = nx.Graph()
+
+    prolific_institutions = \
+        get_prolific_institutions(institutions_d)
+    prolific_institutions= prolific_institutions[0:cut_off]
+
+    nodes = [item[0] for item in prolific_institutions[0:cut_off]]
+
+    edges = list()
+    for inst_1, inst_2 in itertools.combinations(nodes,2):
+        inst_1_papers = set( institutions_d[inst_1]['papers'])
+        inst_2_papers = set(institutions_d[inst_2]['papers'])
+        inter_size = len(inst_1_papers.intersection(inst_2_papers))
+        if inter_size>0:
+            edges.append((inst_1,inst_2))
+
+    G.add_nodes_from(nodes)
+    G.add_edges_from(edges)
+
+    return G
